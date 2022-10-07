9b2f62d32fa3e087e23663df7990a57484f9372a
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-10-07 18:38:08 +0500

    fix: fix social links

--- src/components/data/social.js
+++ src/components/data/social.js
@@ -2,26 +2,21 @@ export const SocialData=[
     {
         platform:"Telegram",
         icon: require("../../assets/icons/telegram.png"),
-        link:"https://web.telegram.org/k/",
+        link:"https://t.me/aht007",
     },
     {
         platform:"Github",
         icon: require("../../assets/icons/github.png"),
-        link:"https://github.com/ThantZinPhyo-OP",
-    },
-    {
-        platform:"Youtube",
-        icon: require("../../assets/icons/youtube.png"),
-        link:"https://www.youtube.com/channel/UCUTEqqa6arBiK3cwDp_jAGA/featured",
+        link:"https://github.com/aht007",
     },
     {
         platform:"Twitter",
         icon: require("../../assets/icons/twitter.png"),
-        link: "https://twitter.com/ThantZinPhyoBR",
+        link: "https://twitter.com/iht_malik",
     },
     {
         platform:"Facebook",
         icon: require("../../assets/icons/facebook.png"),
-        link:"https://www.facebook.com/thantzin.phyo.18",
+        link:"https://www.facebook.com/mohammad.ahtasham",
     },
 ];
