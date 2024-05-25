3273466d5727b3b689212be4382b34b4fad001f2
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2024-05-26 00:11:14 +0500

    feat: work updates

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,20 +1,20 @@
 export const WorkData = [
     {
-        company: "Arbisoft(Subcontracted to edX)",
+        company: "Arbisoft(Contracted to edX)",
         designation: "Software Engineer",
         dateJoining: "July-2021",
         dateEnd: "Present",
         companyLogo: require("../../assets/icons/arbisoft.png"),
-        stack: "Django and Django rest framework, React, Docker, AWS, CI/CD.",
-        work: "Maintaining code dependencies versions, keeping code base up to date, writing new code modernizers, writing and maintaining CI workflows and standardizing the code across edX repos as well as replacing Aging Infrastructure with latest tools and tech"
+        stack: "Django and Django rest framework, React, NextJs, Docker, AWS, CI/CD.",
+        work: "Maintaining and upgrading open edX Codebase, Design and implement new services, Spearheading the new checkout development, Code reviews and Mentoring junior developers"
     },
     {
         company: "Datalatics",
         designation: "Software Engineer",
-        dateJoining: "Jan-2021",
-        dateEnd: "Mar-2021",
+        dateJoining: "Dec-2020",
+        dateEnd: "Jul-2021",
         companyLogo: require('../../assets/icons/datalatics.jpeg'),
         stack:"React, Redux, Node, Express, MongoDB, NestJS, Socket.io",
-        work: "Worked on DeenFund web App frontend in React, Redux and Bootstrap. Wrote some backend APIs in NestJs. Implemented Chat functionality through socket communication using Socket.io in a nodeJS based application."
+        work: "Worked on DeenFund web App frontend in React, Redux and Bootstrap. Wrote backend APIs in NestJs, ExpressJs. Implemented Chat functionality through socket communication using Socket.io in a nodeJS based application."
     },
 ]
